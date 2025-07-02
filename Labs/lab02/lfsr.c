3895e5e235d50e44e3661db229f07a70032b6fbf
smiling689 <2646770377@qq.com>
2025-07-02 16:01:14 +0800

    Lab02 finish

--- Labs/lab02/lfsr.c
+++ Labs/lab02/lfsr.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include "lfsr.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    int bit = ((*reg >> 0) & 1) ^ ((*reg >> 2) & 1) ^ ((*reg >> 3) & 1) ^
+              ((*reg >> 5) & 1);
+    *reg = *reg >> 1;
+    int shift = bit << 15;
+    *reg = shift | (*reg & ~(1 << 15));
 }
-
