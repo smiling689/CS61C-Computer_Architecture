645d33610236d9f20144728cd0253a9c97c33640
smiling689 <2646770377@qq.com>
2025-07-03 13:57:19 +0800

    argmax and dot

--- Project2/src/argmax.s
+++ Project2/src/argmax.s
@@ -15,20 +15,47 @@
 #   this function terminates the program with error code 77.
 # =================================================================
 argmax:
-
     # Prologue
+    addi sp, sp, -8
+    sw s0, 0(sp)
+    sw s1, 4(sp)
 
+    li t0, 1
+    blt a1, t0, wrong
+    li t1, 0
+
+    mv s0, a0
+    mv s1, a1
+
+    lw t2, 0(s0)
+    mv a2, t2 # a2 是最大值
+    mv a3, zero # a3 是最大值索引
+    
 
 loop_start:
-
+    beq t1, s1, loop_end
+    lw t2, 0(s0) 
+    bge a2 , t2, loop_continue
+    mv a2, t2
+    mv a3, t1
 
 loop_continue:
 
+    addi s0, s0, 4
+    addi t1, t1, 1 
+    j loop_start
 
 loop_end:
-    
 
+    mv a0, a3
     # Epilogue
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    addi sp, sp, 8
+    
+	ret
 
 
-    ret
+wrong:
+    li a1, 77
+    jal exit2
